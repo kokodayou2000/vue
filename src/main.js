c10284d45c5409ce939ee9fe475bc2e347bfae7e
kokodayou2000 <1611601667@qq.com>
2022-10-24 21:04:49 +0800

    实现了vue的增删改查

--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 //导入全局设置，去除边框
 import './assets/gloable.css'
+import request from "@/utils/request";
 
 Vue.config.productionTip = false
 
 // Vue.use(ElementUI);
 Vue.use(ElementUI,{size:"mini"});
+
+Vue.prototype.request = request
+
+
 
 new Vue({
   router,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -114,18 +114,39 @@
                       class="ml-5"
                       v-model="email"
             />
-            <el-button class ="ml-5" type="primary" @click="search">搜索</el-button>
+            <el-button class ="ml-5" type="primary" @click="loadPage">搜索</el-button>
+            <el-button class ="ml-5" type="warning" @click="reset">重置</el-button>
           </div>
 
           <div style="margin: 10px 0">
-            <el-button type="primary">新增<i class="el-icon-circle-plus-outline"/></el-button>
-            <el-button type="danger">批量删除<i class="el-icon-remove-outline"/></el-button>
-            <el-button type="primary">导入<i class="el-icon-bottom"/></el-button>
-            <el-button type="primary">导出<i class="el-icon-top"/></el-button>
+            <el-button type="primary" @click="handleAdd"  class="ml-5">新增<i class="el-icon-circle-plus-outline"/></el-button>
+
+            <el-popconfirm
+                class="ml-5"
+                confirm-button-text='确定'
+                cancel-button-text='取消'
+                icon="el-icon-info"
+                icon-color="red"
+                title="确定批量删除吗？"
+                @confirm="delBatch"
+            >
+              <el-button type="danger" slot="reference"  >批量删除<i class="el-icon-remove-outline"/></el-button>
+            </el-popconfirm>
+
+            <el-button type="primary"  class="ml-5">导入<i class="el-icon-bottom"/></el-button>
+            <el-button type="primary"  class="ml-5">导出<i class="el-icon-top"/></el-button>
           </div>
 
 
-          <el-table :data="tableData" border stripe :header-cell-class-name="headerBg">
+          <el-table :data="tableData" border stripe
+                    :header-cell-class-name="headerBg"
+                    @selection-change="handleSelectionChange">
+<!--多选框，为了批量删除-->
+            <el-table-column
+                type="selection"
+                width="55">
+            </el-table-column>
+
 
             <el-table-column prop="id" label="id" width="40">
             </el-table-column>
@@ -141,12 +162,24 @@
             </el-table-column>
 
             <el-table-column prop="option" label="操作">
-              <template>
-                <el-button type="success">编辑<i class="el-icon-edit"/></el-button>
-                <el-button type="danger">删除<i class="el-icon-delete"/></el-button>
+              <template v-slot="scope">
+                <el-button type="success" @click="handleEdit(scope.row)">编辑<i class="el-icon-edit"/></el-button>
+                  <el-popconfirm
+                      class="ml-5"
+                      confirm-button-text='确定'
+                      cancel-button-text='取消'
+                      icon="el-icon-info"
+                      icon-color="red"
+                      title="确定删除吗？"
+                      @confirm="handleDelete(scope.row.id)"
+                  >
+                    <el-button type="danger" slot="reference" >删除<i class="el-icon-delete"/></el-button>
+                  </el-popconfirm>
               </template>
             </el-table-column>
           </el-table>
+
+
           <div style="padding: 10px 0">
 <!--            size-change 和 current-change-->
             <el-pagination
@@ -159,6 +192,50 @@
                 :total="total">
             </el-pagination>
           </div>
+
+<!--          <el-dialog title="用户信息" :visible.sync="dialogTableVisible">-->
+<!--            <el-table :data="gridData">-->
+<!--              <el-table-column property="date" label="日期" width="150"></el-table-column>-->
+<!--              <el-table-column property="name" label="姓名" width="200"></el-table-column>-->
+<!--              <el-table-column property="address" label="地址"></el-table-column>-->
+<!--            </el-table>-->
+<!--          </el-dialog>-->
+
+<!--          &lt;!&ndash; Form &ndash;&gt;-->
+<!--          <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>-->
+
+          <el-dialog title="用户信息" :visible.sync="dialogFormVisible" width="30%">
+            <el-form label-width="70px">
+              <el-form-item label="用户名">
+                <el-input v-model="form.username" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="用户昵称">
+                <el-input v-model="form.nickname" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="邮箱">
+                <el-input v-model="form.email" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="电话">
+                <el-input v-model="form.phone" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="地址">
+                <el-input v-model="form.address" autocomplete="off"></el-input>
+              </el-form-item>
+<!--              <el-form-item label="活动区域" >-->
+<!--                <el-select v-model="form.region" placeholder="请选择活动区域">-->
+<!--                  <el-option label="区域一" value="shanghai"></el-option>-->
+<!--                  <el-option label="区域二" value="beijing"></el-option>-->
+<!--                </el-select>-->
+<!--              </el-form-item>-->
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="cancel">取 消</el-button>
+              <el-button type="primary" @click="save" >确 定</el-button>
+            </div>
+          </el-dialog>
+
+
+
         </el-main>
       </el-container>
     </el-container>
@@ -166,15 +243,18 @@
 
 <script>
 
+import request from "@/utils/request";
+
 export default {
   name: 'Home',
   data(){
     return {
       tableData: [],
       total:0,
+      records:[],
       msg: "Hello deng",
       pageNum:1,
-      pageSize:2,
+      pageSize:5,
       isCollapsed:false,
       collapseBtnClass:'el-icon-s-fold',
       isCollapse:false,
@@ -182,11 +262,19 @@ export default {
       logoTextShow:true,
       headerBg:'headerBg',
       username: "",
-      email:""
+      email:"",
+      nickname:"",
+
+      dialogTableVisible:false,
+      dialogFormVisible:false,
+      form:{},
+      //全局变量记录要删除的集合
+      multipleSelection:[]
     }
   },
   created() {
-    this.loadDataFunction();
+    // this.loadDataFunction();
+    this.loadPage()
   },
   methods:{
     collapse(){
@@ -204,71 +292,95 @@ export default {
     },
     handleCurrentChange(pageNum){
       this.pageNum = pageNum
-      this.selectPage()
+      this.loadPage()
     },
     handleSizeChange(pageSize){
       this.pageSize = pageSize
-      this.selectPage();
+      this.loadPage()
     },
-    selectPage(){
-      if (this.username === "" && this.email === ""){
-        this.loadDataFunction()
-      }else if(this.username === "" && this.email !== ""){
-        this.selectByEmail()
-      }else if(this.username !== "" && this.email === ""){
-        this.searchByName()
-      }else{
-        this.searchByUsernameAndEmail()
-      }
-    },
-    search(){
-      if (this.username === "" && this.email === ""){
-        this.loadDataFunction()
-      }else if(this.username === "" && this.email !== ""){
-        this.selectByEmail()
-      }else if(this.username !== "" && this.email === ""){
-        this.searchByName()
-      }else{
-        this.searchByUsernameAndEmail()
-      }
-    },
-    searchByName(){
-      fetch("http://localhost:8081/user/pageByUsername?pageNum="+this.pageNum+"&pageSize="+this.pageSize+"&username="+this.username)
-          .then(
-              res=>res.json() //将字符串转换为json
-          ).then(res => {
-        this.tableData = res.data;
-        this.total = res.total;
+    //使用axios的优势的能在request里面设置一些通用数据，可以更好的设置参数
+    loadPage(){
+      request.get("/user/page",{
+        params:{
+          pageNum:this.pageNum,
+          pageSize:this.pageSize,
+          username:this.username,
+          email:this.email
+        }
       })
+          .then(res => {
+            this.tableData = res.records
+            this.total = res.total
+          })
+
+      // fetch("http://localhost:8081/user/page?pageNum="+this.pageNum+"&pageSize="+this.pageSize+"&username="+this.username+"&email="+this.email)
+      //     .then(
+      //         res=>res.json() //将字符串转换为json
+      //     ).then(res => {
+      //   this.tableData = res.records;
+      //   this.total = res.total;
+      // })
+      console.log("username= "+this.username+"  email= "+this.email)
     },
-    searchByUsernameAndEmail(){
-      fetch("http://localhost:8081/user/searchByUsernameAndEmail?pageNum="+this.pageNum+"&pageSize="+this.pageSize+"&username="+this.username+"&email="+this.email)
-          .then(
-              res=>res.json() //将字符串转换为json
-          ).then(res => {
-        this.tableData = res.data;
-        this.total = res.total;
-      })
+    reset(){
+      this.username=""
+      this.email=""
+      this.loadPage()
     },
-    loadDataFunction(){
-      //请求分页查询数据
-      fetch("http://localhost:8081/user/page?pageNum="+this.pageNum+"&pageSize="+this.pageSize)
-          .then(
-              res=>res.json() //将字符串转换为json
-          ).then(res => {
-        this.tableData = res.data;
-        this.total = res.total;
-      })
+    handleAdd(){
+      //显示新增页
+      this.dialogFormVisible=true
+      this.form={}
     },
-    selectByEmail() {
-      //请求分页查询数据
-      fetch("http://localhost:8081/user/pageByEmail?pageNum="+this.pageNum+"&pageSize="+this.pageSize+"&email="+this.email)
-          .then(
-              res=>res.json() //将字符串转换为json
-          ).then(res => {
-        this.tableData = res.data;
-        this.total = res.total;
-      })
+    save(){
+      request.post("/user",this.form).then( res=>{
+        if (res){
+          this.$message.success("保存成功")
+          this.dialogFormVisible = false
+          this.loadPage()
+        }else{
+          this.$message.error("保存失败")
+        }
+        })
+    },
+    cancel(){
+      this.loadPage()
+      this.dialogFormVisible = false
+    },
+    handleEdit(row){
+      this.form = row
+      this.dialogFormVisible = true
+    },
+    handleDelete(id){
+      request.delete("/user/"+id)
+          .then(res =>{
+            if(res){
+              this.$message.success("删除成功")
+              this.loadPage()
+            }else{
+              this.$message.error("删除失败")
+            }}
+          )
+    },
+    handleSelectionChange(val){
+      //val就对应的复选框选择的数据
+      this.multipleSelection=val
+    },
+    delBatch(){
+      //执行批量删除的方法
+      //将对象数组变成id的数组 [object1{},object2{},object3{}] -> [id1,id2,id3]
+      let ids = this.multipleSelection.map(v => v.id)
+
+      request.post("/user/del/batch",ids)
+          .then(res =>{
+            if(res){
+              this.$message.success("批量删除成功")
+              this.loadPage()
+            }else{
+              this.$message.error("批量删除失败")
+            }}
+          )
+
     }
   }
 
